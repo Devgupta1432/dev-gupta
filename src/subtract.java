6a5d594b87aea8e1763ceb495238fce1dc4260f8
Devgupta1432 <devgupta791@gmail.com>
2025-01-17 14:50:33 +0530

    Sub updated

--- src/subtract.java
+++ src/subtract.java
@@ -5,8 +5,8 @@ public class subtract {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = a + b;
-        System.out.println(sum);
+        int sub = a - b;
+        System.out.println(sub);
 
 
     }
